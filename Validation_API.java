3c78cdcaa9a4f19c38bd414f682a4a0c4c819b24
atul-2000 <51433658+atul-2000@users.noreply.github.com>
2020-05-20 16:12:20 +0530

    Update Validation_API.java

--- Validation_API.java
+++ Validation_API.java
@@ -4,14 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 public class trial {
 
-	public UUID handleRequest(List<String> input) {
+	public String handleRequest(List<String> input) {
         String name=null,id=null;
+        String out ="Your query has been noted successfully and Unique token associated with it is ";
         name=input.get(0);
         id=input.get(1);
         UUID uuid = UUID.randomUUID();
-        System.out.println("Validation API called with parameters name:"+name+","+ " seller I'd:"+id);
-        System.out.println("Unique token associated with this query:"+uuid);
-	return uuid;
+		System.out.println("Validation API called with parameters name:"+name+","+ " seller I'd:"+id);
+		System.out.println("Unique token associated with this query:"+uuid);
+	    return out+uuid.toString();
 	  } 
 
 	}
+
